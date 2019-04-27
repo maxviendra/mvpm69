592cd717f7d165fa0aa2bf6f799e2597084347b5
maxviendra <49405335+maxviendra@users.noreply.github.com>
2019-04-27 22:47:34 +0700

    flash
    none

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid="a1a7bcaa-59df-4ed0-98a7-fe350f61a906";
+//user_token wajib di isi //
+$user_token="AbyuOrzVToDkvCjh";
+
+//user_id wajib di isi //
+$user_id="Facebook_2197398417190398";
+
+// invite_code wajib di isi //
+$invite_code="FA3yQkHiVq";
+
+// Bearer wajib di isi //
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18yMTk3Mzk4NDE3MTkwMzk4IiwiZXhwIjoiMTU1MTQwMTcyNDE5MiJ9.Wdgozu4K37UrrLtWEbOmeR0MS1laWhL9EVjW4bylYG4";
+
+
+
+?>
